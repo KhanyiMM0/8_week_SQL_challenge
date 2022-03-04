cf82fe54ba6d2e4c27a2cc551a86775f5897d59e
KhanyiMM <moyo.khanyi@gmail.com>
2022-03-04 09:36:42 +0200

    runner and customer experience

--- pizza_runner/table_query.sql
+++ pizza_runner/table_query.sql
@@ -160,3 +160,27 @@ SELECT
 FROM pizza_runner.customer_orders
 GROUP BY 1
 ORDER BY 2
+
+----------- A. Runner and Customer Experience ------------------
+
+-- 1. How many runners signed up for each 1 week period? (i.e. week starts 2021-01-01)
+SELECT
+	TO_CHAR(registration_date, 'WW')::INTEGER registration_week,
+	COUNT(*) total_registered_runners
+FROM pizza_runner.runners
+GROUP BY 1
+ORDER BY 1
+
+-- 2. What was the average time in minutes it took for each runner to arrive at the Pizza Runner HQ to pickup the order?
+select
+	runner_id,
+    ROUND(AVG(EXTRACT('minute' FROM pickup_time)),1) average_pickup_time
+from pizza_runner.runner_orders
+GROUP BY 1
+ORDER BY 2
+
+-- 3. Is there any relationship between the number of pizzas and how long the order takes to prepare?
+-- 4. What was the average distance travelled for each customer?
+-- 5. What was the difference between the longest and shortest delivery times for all orders?
+-- 6. What was the average speed for each runner for each delivery and do you notice any trend for these values?
+-- 7. What is the successful delivery percentage for each runner?
